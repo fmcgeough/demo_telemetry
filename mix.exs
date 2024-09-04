99c29c990e0cf5315e286cba742d5cef0d368d1a
Frank McGeough <fmcgeough@gmail.com>
2024-09-04 18:52:52 -0400

    Update required Elixir for project

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DemoTelemetry.MixProject do
     [
       app: :demo_telemetry,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
