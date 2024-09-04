c0e81bf174363a61d12ad8091ac937997e47a4d5
Frank McGeough <fmcgeough@gmail.com>
2024-09-04 18:55:09 -0400

    Fix credo warning

--- lib/demo_telemetry/database/other_repo.ex
+++ lib/demo_telemetry/database/other_repo.ex
@@ -1,4 +1,7 @@
 defmodule DemoTelemetry.Database.OtherRepo do
+  @moduledoc """
+  Repo to demonstrate Ecto event named using Ecto.Repo module name
+  """
   use Ecto.Repo,
     otp_app: :demo_telemetry,
     adapter: Ecto.Adapters.Postgres
